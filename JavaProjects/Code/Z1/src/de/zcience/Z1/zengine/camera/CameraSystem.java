0d8552bb610e9b80b2698b87acc26be2b2caaf63
Zcience <david.liebemann@gmail.com>
2015-08-23 06:55:57 +0200

    lovin it first i hated. didn't work now its pretty nice. le smooooothz cameraz

--- JavaProjects/Code/Z1/src/de/zcience/Z1/zengine/camera/CameraSystem.java
+++ JavaProjects/Code/Z1/src/de/zcience/Z1/zengine/camera/CameraSystem.java
@@ -24,11 +24,14 @@ import de.zcience.Z1.zengine.util.GameConstants;
 public class CameraSystem extends EntitySystem implements EntityListener {
 
 	/**
-	 * target that should be followed by the CameraSystem
+	 * target that should be followed by the CameraSystem. Camera will be
+	 * smoothed out by giving it the behavior of a spring
 	 */
 	private Entity target;
 
 	private OrthographicCamera camera;
+	private Vector2 camVelocity = new Vector2();
+	private float dampConst, springConst, massCoefficient, cameraSmoothnessX, cameraSmoothnessY;
 
 	public Vector2 viewpoint = new Vector2();
 
@@ -37,14 +40,52 @@ public class CameraSystem extends EntitySystem implements EntityListener {
 		camera = new OrthographicCamera();
 		resizeCameraViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
+		setSpringConst(5f);
+		setMass(1.0f);
+		setCameraSmoothness(0.5f, 2.0f);
+	}
+
+	public void setSpringConst(float springConst) {
+		// choose the springConst by your own will
+		this.springConst = springConst;
+		// use critical damping to counteract oscillating of the camera
+		this.dampConst = (float) (2.0 * Math.sqrt(springConst));
+	}
+
+	public void setMass(float mass) {
+		// because division is slow, we will just compute it once and store it
+		// for use in force calculation
+		this.massCoefficient = 1.0f / mass;
+	}
+	
+	public void setCameraSmoothness(float cameraSmoothnessX, float cameraSmoothnessY){
+		this.cameraSmoothnessX = cameraSmoothnessX;
+		this.cameraSmoothnessY = cameraSmoothnessY;
 	}
 
 	@Override
 	public void update(float deltaTime) {
 		if (target != null) {
 			PositionComponent posComp = CompMappers.position.get(target);
+			
+
 			if (posComp != null) {
-				setCameraPosition(posComp.x, posComp.y);
+				Vector2 force = new Vector2();
+				Vector2 camPos = getCameraPosition();
+
+				force.x = ((springConst * (posComp.x - camPos.x)) - (dampConst * camVelocity.x)) * massCoefficient;
+				force.y = ((springConst * (posComp.y - camPos.y)) - (dampConst * camVelocity.y)) * massCoefficient;
+				// integrate camVelocity
+				camVelocity.x += force.x * deltaTime;
+				camVelocity.y += force.y * deltaTime;
+				// integrate position
+				float integrateX = camVelocity.x * deltaTime;
+				float integrateY = camVelocity.y * deltaTime;
+				if(Math.abs(integrateX) > cameraSmoothnessX || Math.abs(integrateY) > cameraSmoothnessY){
+					camPos.x +=  integrateX;
+					camPos.y +=  integrateY;
+				}	
+				setCameraPosition(camPos.x, camPos.y);
 			}
 		}
 		camera.update();
@@ -52,9 +93,7 @@ public class CameraSystem extends EntitySystem implements EntityListener {
 
 	@Override
 	public void addedToEngine(Engine engine) {
-		engine.addEntityListener(
-				Family.all(PlayerComponent.class, PositionComponent.class)
-						.get(), this);
+		engine.addEntityListener(Family.all(PlayerComponent.class, PositionComponent.class).get(), this);
 	}
 
 	@Override
@@ -70,6 +109,15 @@ public class CameraSystem extends EntitySystem implements EntityListener {
 		target = null;
 	}
 
+	/**
+	 *
+	 * @return position in pixel coordinates
+	 */
+	public Vector2 getCameraPosition() {
+		return new Vector2(camera.position.x * GameConstants.BOX2D_SCALE,
+				camera.position.y * GameConstants.BOX2D_SCALE);
+	}
+
 	/* CameraControls */
 	/**
 	 * Resizes the Camera Viewport. It will automatically be downscaled by the
@@ -79,8 +127,7 @@ public class CameraSystem extends EntitySystem implements EntityListener {
 	 * @param height
 	 */
 	public void resizeCameraViewport(int width, int height) {
-		camera.setToOrtho(GameConstants.YDOWN, width
-				/ GameConstants.BOX2D_SCALE, height / GameConstants.BOX2D_SCALE);
+		camera.setToOrtho(GameConstants.YDOWN, width / GameConstants.BOX2D_SCALE, height / GameConstants.BOX2D_SCALE);
 		camera.update(true);
 	}
 
@@ -92,8 +139,10 @@ public class CameraSystem extends EntitySystem implements EntityListener {
 	 * Sets the Camera Position to the given position, automatically scales the
 	 * position by the BOX2D_SCALE factor.
 	 * 
-	 * @param x vertical Position
-	 * @param y horizontal Position
+	 * @param x
+	 *            vertical Position
+	 * @param y
+	 *            horizontal Position
 	 */
 	public void setCameraPosition(float x, float y) {
 		camera.position.x = x / GameConstants.BOX2D_SCALE;
